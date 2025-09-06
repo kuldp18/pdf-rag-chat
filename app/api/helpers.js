9a9efc0ab22c62ae5a1dbdbb686ce39d2f323f87
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2025-09-07 00:03:34 +0530

    add more helpers

--- app/api/helpers.js
+++ app/api/helpers.js
@@ -37,6 +37,28 @@ export async function createEmbeddings(chunks, filename) {
   }
 }
 
+export async function createQueryVector(queryText) {
+  if (!queryText || typeof queryText !== "string") {
+    throw new Error("Invalid text provided for creating query embedding");
+  }
+  try {
+    const res = await openai.embeddings.create({
+      model: "text-embedding-3-small",
+      input: queryText,
+      encoding_format: "float",
+    });
+
+    if (!res.data.length) {
+      throw new Error("OpenAI failed while creating query vector");
+    }
+
+    return res.data[0].embedding;
+  } catch (error) {
+    console.error("OpenAI embeddings error:", error);
+    throw new Error("Failed to create query vector from OpenAI");
+  }
+}
+
 export async function saveEmbeddings(embeddings) {
   if (!embeddings.length) throw new Error("No embeddings provided");
 
@@ -60,5 +82,33 @@ export async function saveEmbeddings(embeddings) {
   } catch (error) {
     console.error("Error while saving embeddings:", error);
     throw error;
+  }
+}
+
+export async function queryEmbeddings(queryVector, filename, topK = 5) {
+  try {
+    const res = await fetch(`${process.env.UPSTASH_ENDPOINT}/query`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.UPSTASH_API_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        vector: queryVector,
+        topK,
+        filter: { source: filename }, //filter relevant pdf
+      }),
+    });
+
+    if (!res.ok) {
+      const errText = await res.text();
+      throw new Error(`Upstash query failed: ${errText}`);
+    }
+
+    const data = await res.json();
+    return data.results || [];
+  } catch (err) {
+    console.error("Error querying Upstash:", err);
+    throw err;
   }
 }
